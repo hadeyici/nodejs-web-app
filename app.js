fd088316bbd56c375da96f4dcdc3de31eeadda7b
Hatice Ergün <hadeyici@gmail.com>
2020-12-19 20:41:14 +0300

    express and handlebars packages added

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Index Route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//About Route
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
