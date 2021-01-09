42e6fa6d841e62832080ce47313e6931f846a4a5
Hatice Ergün <hadeyici@gmail.com>
2021-01-09 20:48:38 +0300

    body parser added and article post route added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -21,6 +22,10 @@ const Article = mongoose.model("articles");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Body parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Index Route
 app.get("/", (req, res) => {
   res.render("index");
@@ -39,6 +44,36 @@ app.get("/articles", (req, res) => {
 //Add article Form
 app.get("/articles/add", (req, res) => {
   res.render("articles/add");
+});
+
+//Article Route
+app.post("/articles", (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: "title is required" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "detail is required" });
+  }
+
+  if (errors.length > 0) {
+    res.render("/articles/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details,
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details,
+    };
+    new Article(newUser)
+    .save()
+    .then(article => {
+      res.redirect('/articles');
+    } )
+  }
 });
 
 //Contact Route
